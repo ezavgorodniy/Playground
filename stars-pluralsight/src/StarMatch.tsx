c00e7104484f973b8a68b9d9170c22ea97334ea5
Yevhen Zavhorodnii <eugurzav@mail.ru>
2020-01-18 22:17:22 +0000

    Revert "(WIP) NavBar" - React part
    This reverts commit 3b259935f9119a426e2db37c6e69cd7da38f9284.

--- stars-pluralsight/src/StarMatch.tsx
+++ stars-pluralsight/src/StarMatch.tsx
@@ -1,26 +1,11 @@
 import React, { useState } from 'react';
 import './StarMatch.css';
-import { range, random, sum } from './Utils';
+import { range, random } from './Utils';
 import PlayNumber from './PlayNumber';
 import StarsDisplay from './StarsDisplay';
 
 const StarMatch: React.FC = () => {
   const [stars, setStars] = useState(random(1, 9));
-  const [availableNums, setAvailableNums] = useState([1,2,3,4,5]);
-  const [candidateNums, setCandidateNums] = useState([2,3]);
-
-  const candidatesAreWrong = sum(candidateNums) > stars;
-
-  const numberStatus = (number: number) => {
-    if (!availableNums.includes(number)) {
-      return 'used';
-    }
-    if (candidateNums.includes(number)) {
-      return candidatesAreWrong ? 'wrong' : 'candidate';
-    }
-    return 'available';
-  };
-
   return (
     <div className="game">
       <div className="help">
@@ -32,11 +17,7 @@ const StarMatch: React.FC = () => {
         </div>
         <div className="right">
           {range(1, 9).map(number => 
-            <PlayNumber 
-              key={number}
-              number={number}
-              status={numberStatus(number)} 
-            />
+            <PlayNumber number={number} />
           )}
         </div>
       </div>
